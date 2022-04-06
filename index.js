50531b7e6c3a5317184f672807fd4215a410aad9
Penelope Durand <penelope.durand@mail.goucher.edu>
2022-04-06 17:01:17 -0400

    commit tree c8781e597cf3f7a286fe28a61ee4491d0183013f

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching (drivers, string) {
+    return drivers.filter(function(driverName) {
+        return driverName.toLowerCase() === string.toLowerCase();
+    })
+}
+
+function fuzzyMatch (drivers, string) {
+    return drivers.filter(function (driverName) {
+        return driverName.slice(0, string.length) === string;
+    })
+}
+
+function matchName (drivers, string) {
+    return drivers.filter(function(driverOb) {
+        return driverOb.name === string;
+    })
+}
